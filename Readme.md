5aad23f2538451906fc3f04227df22b9f8348c14
Friedrich von Never <friedrich@fornever.me>
2017-01-15 22:32:01 +0700

    Console: initial implementation

--- Readme.md
+++ Readme.md
@@ -3,3 +3,6 @@ W3gParser
 
 W3gParser is a .NET library to parse WarCraft III: The Frozen Throne replays to
 extract useful information from the replays.
+
+It includes a terminal tool called `W3gParser.Console` to print the extracted
+information.
---
+++ W3gParser.Console/Program.fs
@@ -0,0 +1,15 @@
+﻿module W3gParser.Console.Program
+
+open Deerchao.War3Share.W3gParser
+
+[<EntryPoint>]
+let main argv =
+    if argv.Length < 1
+    then
+        printfn "Usage: W3gParser.Console <replay-filepath>"
+        1
+    else
+        let r = Replay(argv.[0])
+        r.Players
+        |> Seq.iter (fun p -> printfn "%s: %d actions / %A = %g APM" p.Name p.Actions (p.GetTime()) p.Apm)
+        0
