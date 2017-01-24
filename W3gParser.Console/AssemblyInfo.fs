80d03f22eddde7890abbf1185f97f2133002b6d6
Friedrich von Never <friedrich@fornever.me>
2017-01-24 20:47:46 +0700

    AssemblyInfo: update

--- W3gParser.Console/AssemblyInfo.fs
+++ W3gParser.Console/AssemblyInfo.fs
@@ -1,41 +1,14 @@
 ﻿namespace W3gParser.Console.AssemblyInfo
 
 open System.Reflection
-open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [<assembly: AssemblyTitle("W3gParser.Console")>]
-[<assembly: AssemblyDescription("")>]
-[<assembly: AssemblyConfiguration("")>]
-[<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("W3gParser.Console")>]
-[<assembly: AssemblyCopyright("Copyright ©  2017")>]
-[<assembly: AssemblyTrademark("")>]
-[<assembly: AssemblyCulture("")>]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[<assembly: AssemblyProduct("W3gParser")>]
+[<assembly: AssemblyCopyright("Copyright © Friedrich von Never 2017")>]
 [<assembly: ComVisible(false)>]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [<assembly: Guid("5dc3a7de-1126-4fab-89c7-8d936c0491ec")>]
-
-// Version information for an assembly consists of the following four values:
-// 
-//       Major Version
-//       Minor Version 
-//       Build Number
-//       Revision
-// 
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [<assembly: AssemblyVersion("1.0.*")>]
 [<assembly: AssemblyVersion("1.0.0.0")>]
 [<assembly: AssemblyFileVersion("1.0.0.0")>]
 
-do
-    ()
+()
